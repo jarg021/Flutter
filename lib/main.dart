ee0ec7825304105500870f9632f2319c65faa022
jarg021 <31014497+jarg021@users.noreply.github.com>
2020-04-19 18:03:19 -0500

    Add Text and buttons inside column

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,25 @@
-
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(MyApp());
 }
-class MyApp extends StatelessWidget{
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: Text("Hola Mundo"),),
-      body:  Text("Este es mi primer texto"),
-    ),);
+    //var Questions=[ "What is your favorite Color?", "What is your favorite animal?"]
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hola Mundo"),
+        ),
+        body:Column(children: [
+        Text("The question"),
+        RaisedButton(child:Text("Answer 1"),onPressed: null),
+        RaisedButton(child:Text("Answer 2"),onPressed: null),
+        RaisedButton(child:Text("Answer 3"),onPressed: null),
+        ]),
+      ),
+    );
   }
 }
